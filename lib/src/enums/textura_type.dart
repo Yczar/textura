66be98ef07a76919578840b05e2dfe64c700f6bf
Yczar <babalolagbogo@gmail.com>
2023-10-25 12:13:25 +0100

    chore: create textura type

---
+++ lib/src/enums/textura_type.dart
@@ -0,0 +1,186 @@
+/// Enum representing the different types of textures available.
+/// Each value corresponds to a unique texture that can be applied as a
+///  background.
+///
+/// Sample Usage:
+/// ```dart
+/// Textura(
+///   textureType: TextureType.asphalt,
+///   child: YourWidget(),
+/// )
+/// ```
+///
+/// In this example, `YourWidget()` will be rendered with the asphalt texture
+/// applied as the background.
+enum TextureType {
+  /// Asphalt texture
+  asphalt,
+
+  /// Brick Wall texture
+  brickWall,
+
+  /// Bubble texture
+  bubble,
+
+  /// Camouflage texture
+  camo,
+
+  /// Canvas texture
+  canvas,
+
+  /// Celtic Knots texture
+  celticKnots,
+
+  /// Chequered texture
+  chequered,
+
+  /// Chessboard texture
+  chessboard,
+
+  /// Circuitry texture
+  circuitry,
+
+  /// City Maps texture
+  cityMaps,
+
+  /// Clouds texture
+  clouds,
+
+  /// Concrete texture
+  concrete,
+
+  /// Constellations texture
+  constellations,
+
+  /// Denim texture
+  denim,
+
+  /// DNA texture
+  dna,
+
+  /// Fabric Texture
+  fabricTexture,
+
+  /// Foam texture
+  foam,
+
+  /// Furry texture
+  furry,
+
+  /// Galaxy texture
+  galaxy,
+
+  /// Game Pixels texture
+  gamePixels,
+
+  /// Glitch texture
+  glitch,
+
+  /// Graffiti texture
+  graffiti,
+
+  /// Granite texture
+  granite,
+
+  /// Graph Paper texture
+  graphPaper,
+
+  /// Grass texture
+  grass,
+
+  /// Greek Keys texture
+  greekKeys,
+
+  /// Grid texture
+  grid,
+
+  /// Holographic texture
+  holographic,
+
+  /// Honeycomb texture
+  honeycomb,
+
+  /// Leather texture
+  leather,
+
+  /// Leaves texture
+  leaves,
+
+  /// Leopard texture
+  leopard,
+
+  /// Linen texture
+  linen,
+
+  /// Marble texture
+  marble,
+
+  /// Metal Texture
+  metalTexture,
+
+  /// Mosaic texture
+  mosaic,
+
+  /// Mud texture
+  mud,
+
+  /// Music Sheet texture
+  musicSheet,
+
+  /// Newsprint texture
+  newsprint,
+
+  /// Paper texture
+  paper,
+
+  /// Perforated Metal texture
+  perforatedMetal,
+
+  /// Roadways texture
+  roadways,
+
+  /// Rubber texture
+  rubber,
+
+  /// Rusted Metal texture
+  rustedMetal,
+
+  /// Sand texture
+  sand,
+
+  /// Silk texture
+  silk,
+
+  /// Solar System texture
+  solarSystem,
+
+  /// Spider Web texture
+  spiderWeb,
+
+  /// Spirals texture
+  spirals,
+
+  /// Star texture
+  star,
+
+  /// Steel texture
+  steel,
+
+  /// Suede texture
+  suede,
+
+  /// Sunset texture
+  sunset,
+
+  /// Train Tracks texture
+  trainTracks,
+
+  /// Water texture
+  water,
+
+  /// Wood Grain texture
+  woodGrain,
+
+  /// Zebra texture
+  zebra,
+}
