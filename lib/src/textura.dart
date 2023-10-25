dddfbeb41dbca3afbd7ed7b076db2926d3633e6f
Yczar <babalolagbogo@gmail.com>
2023-10-25 12:13:05 +0100

    chore: create textura

---
+++ lib/src/textura.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:textura/src/enums/textura_type.dart';
+import 'package:textura/src/extensions/texture_type_extension.dart';
+
+/// `Textura` is a widget that applies a texture as a background to its child
+/// widget.
+/// It takes a `TextureType` as a required parameter to determine which texture
+/// to apply.
+///
+/// Sample Usage:
+/// ```dart
+/// Textura(
+///   textureType: TextureType.asphalt,
+///   child: YourWidget(),
+/// )
+/// ```
+/// In this usage, `YourWidget()` will be displayed with an asphalt texture
+/// applied as its background.
+class Textura extends SingleChildRenderObjectWidget {
+  /// Constructs a `Textura` widget.
+  ///
+  /// The [textureType] determines which texture is applied as a background to
+  /// the [child].
+  const Textura({
+    required this.textureType,
+    super.key,
+    super.child,
+  });
+
+  /// The type of texture to be applied.
+  final TextureType textureType;
+
+  /// Creates the `RenderObject` that applies the texture.
+  ///
+  /// It fetches the appropriate `RenderBox` based on [textureType]
+  /// to render the texture.
+  @override
+  RenderObject createRenderObject(BuildContext context) {
+    return textureType.renderObject;
+  }
+}
